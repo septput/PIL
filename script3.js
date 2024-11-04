557ba18cd83e6442ce33abbef894fb70367e8d0b
Septriane Putri Akprita <48571041+septput@users.noreply.github.com>
2024-11-05 01:46:34 +0800

    Update script3.js

--- script3.js
+++ script3.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const signatureDataUrl = canvas.toDataURL("image/png");
 
         try {
-            const response = await fetch("https://script.google.com/macros/s/AKfycbwbu_MgyJxo-HyR5dpeYAoRymfL07qwv7Lj4Sa8UFE/exec", {
+            const response = await fetch("https://script.google.com/macros/s/AKfycbx4k41v-fGhrN8n7GHvXJp3YR2Um3Oqo9VHQFglO7jSdocxi2c2dqCFD2IIaHTFHMKE/exec", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
